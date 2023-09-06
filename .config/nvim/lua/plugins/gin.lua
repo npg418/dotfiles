5cfc214e222e553b29a5630a858f237fc00503d1
NPG418 <narupenguin919@gmail.com>
2023-09-06 16:02:09 +0900

    gin.vimを追加

---
+++ .config/nvim/lua/plugins/gin.lua
@@ -0,0 +1,19 @@
+return {
+  'lambdalisue/gin.vim',
+  cmd = {
+    'Gin',
+    'GinBuffer',
+    'GinBranch',
+    'GinBrowse',
+    'GinCd',
+    'GinLcd',
+    'GinTcd',
+    'GinChaperon',
+    'GinDiff',
+    'GinEdit',
+    'GinLog',
+    'GinPatch',
+    'GinStatus',
+  },
+  dependencies = 'vim-denops/denops.vim',
+}
