e8373f18c1785056fe863c2fba1a8fcb7b191f6c
NPG418 <narupenguin919@gmail.com>
2023-10-31 10:31:16 +0900

    切り替えのキーマップ

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -4,3 +4,12 @@ vim.cmd [[cnoreabbrev <expr> s getcmdtype() .. getcmdline() ==# ':s' ? [getchar(
 -- root権限で保存
 vim.keymap.set('c', 'w!!', 'w !sudo tee > /dev/null %')
 
+-- bufferの切り替え
+vim.keymap.set('n', '<C-l>', '<cmd>bnext<CR>')
+vim.keymap.set('n', '<C-h>', '<cmd>bprevious<CR>')
+
+-- ペインの切り替え
+vim.keymap.set('n', '<A-j>', '<C-w>j')
+vim.keymap.set('n', '<A-k>', '<C-w>k')
+vim.keymap.set('n', '<A-h>', '<C-w>h')
+vim.keymap.set('n', '<A-l>', '<C-w>l')
