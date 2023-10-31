6372986c73c76c4c6151b5c3c7e5fe43cb5339ed
NPG418 <narupenguin919@gmail.com>
2023-10-31 13:20:44 +0900

    treesitter使ってみる

---
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -0,0 +1,22 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  run = ':TSUpdate',
+  event = 'BufRead',
+  dependencies = {
+    'windwp/nvim-ts-autotag',
+  },
+  config = function ()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = { 'lua' },
+      sync_install = true,
+      auto_install = true,
+
+      highlight = {
+        enable = true,
+      },
+      autotag = {
+        enable = true,
+      },
+    }
+  end,
+}
