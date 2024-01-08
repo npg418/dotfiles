ac056fc633da5a1331c1ced7f63a2a119a15ada2
NPG418 <narupenguin919@gmail.com>
2024-01-08 13:39:19 +0900

    ターミナルをもっと使いやすく

--- .config/nvim/lua/terminal.lua
+++ .config/nvim/lua/terminal.lua
@@ -1,25 +1,30 @@
-vim.api.nvim_create_user_command('T', 'split | wincmd j | resize 20 | terminal', {})
-
 vim.api.nvim_create_augroup('terminal', {})
 vim.api.nvim_create_autocmd('TermOpen', {
   group = 'terminal',
   callback = function()
     vim.opt_local.relativenumber = false
     vim.opt_local.number = false
-    vim.opt_local.bufhidden = 'hide'
-    vim.cmd('startinsert')
+    vim.opt_local.bufhidden = 'wipe'
+    vim.cmd.startinsert()
   end
 })
 vim.api.nvim_create_autocmd('TermClose', {
   group = 'terminal',
   callback = function()
-    vim.cmd('bwipe')
+    vim.cmd.close()
+  end
+})
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+  group = 'terminal',
+  pattern = 'term://*',
+  callback = function()
+    vim.cmd.startinsert()
   end
 })
 
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
-vim.keymap.set('n', '<A-t>', ':T<CR>')
-vim.keymap.set('t', '<A-t>', '<C-c> exit<CR>')
+vim.keymap.set('n', '<A-t>', ':split | wincmd j | resize 20 | terminal<CR>')
+vim.keymap.set('t', '<A-t>', '<C-c> exit<CR>:q<CR>')
 
 if vim.fn.executable('pwsh') == 1 then
   vim.o.shell = 'pwsh'
