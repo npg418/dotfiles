3aa75f176b838bd89a71abf55fecabc64cc3c352
NPG418 <narupenguin919@gmail.com>
2024-01-26 09:09:22 +0900

    ペインを消すショートカット

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -9,7 +9,10 @@ vim.keymap.set('n', '<C-l>', '<cmd>bnext<CR>')
 vim.keymap.set('n', '<C-h>', '<cmd>bprevious<CR>')
 
 -- ペインの切り替え
-vim.keymap.set('n', '<A-j>', '<C-w>j')
-vim.keymap.set('n', '<A-k>', '<C-w>k')
-vim.keymap.set('n', '<A-h>', '<C-w>h')
-vim.keymap.set('n', '<A-l>', '<C-w>l')
+vim.keymap.set('n', '<A-j>', '<cmd>wincmd j<CR>')
+vim.keymap.set('n', '<A-k>', '<cmd>wincmd k<CR>')
+vim.keymap.set('n', '<A-h>', '<cmd>wincmd h<CR>')
+vim.keymap.set('n', '<A-l>', '<cmd>wincmd l<CR>')
+
+-- ペインを削除
+vim.keymap.set('n', '<C-x>', '<cmd>quit<CR>')
