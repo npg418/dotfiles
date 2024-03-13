6ff4c32ec09a2ca9b8c225a9893daaf54d407abb
NPG418 <narupenguin919@gmail.com>
2024-03-13 11:57:57 +0900

    ambiwidthで絵文字をきれいに

---
+++ .config/nvim/lua/plugins/ambiwidth.lua
@@ -0,0 +1,11 @@
+vim.g.ambiwidth_cica_enabled = false
+---@type LazySpec
+return {
+  'rbtnn/vim-ambiwidth',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    vim.fn.setcellwidths({
+      { 0x276F, 0x276F, 1 },
+    })
+  end,
+}
