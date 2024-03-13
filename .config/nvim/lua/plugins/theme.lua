54c6d94e9da49bf5528a3c2c15f3dedab8e80cbf
NPG418 <narupenguin919@gmail.com>
2024-03-13 12:25:04 +0900

    themeの設定直した

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,31 +1,32 @@
+---@type LazySpec
 return {
   {
     'morhetz/gruvbox',
     lazy = false,
-    enable = false,
     priority = 1000,
+    enabled = false,
     config = function()
-      -- vim.cmd.colorscheme 'gruvbox'
+      vim.cmd.highlight('gruvbox')
     end,
   },
   {
     'folke/tokyonight.nvim',
     lazy = false,
-    enable = false,
     priority = 1000,
+    enabled = false,
     config = function()
-      -- vim.cmd.colorscheme 'tokyonight'
+      vim.cmd.highlight('tokyonight')
     end,
   },
   {
     '4513ECHO/vim-colors-hatsunemiku',
     lazy = false,
-    enable = true,
     priority = 1000,
+    enabled = true,
     config = function()
       vim.cmd.colorscheme('hatsunemiku')
       vim.o.termguicolors = true
-      vim.cmd.highlight { 'link VertSplit NonText', bang = true }
+      vim.cmd.highlight({ 'link VertSplit NonText', bang = true })
       vim.o.fillchars = 'eob: ,vert:┃'
     end,
   },
