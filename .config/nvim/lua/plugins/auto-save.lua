aea4beb71a7043e4400ba098a66a3401852f3672
NPG418 <narupenguin919@gmail.com>
2024-03-19 08:27:41 +0900

    new fileを自動保存しないように調整

--- .config/nvim/lua/plugins/auto-save.lua
+++ .config/nvim/lua/plugins/auto-save.lua
@@ -1,9 +1,11 @@
+---@type LazySpec
 return {
   'pocco81/auto-save.nvim',
-  event = { 'BufLeave', 'FocusLost' },
-  config = function()
-    require('auto-save').setup({
-      trigger_events = { 'BufLeave', 'FocusLost' },
-    })
-  end,
+  event = { 'FocusLost', 'BufLeave' },
+  opts = {
+    trigger_events = { 'FocusLost', 'BufLeave' },
+    condition = function()
+      return vim.fn.filereadable(vim.fn.expand('%')) == 1
+    end,
+  },
 }
