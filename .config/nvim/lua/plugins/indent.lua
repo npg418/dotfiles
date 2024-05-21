c5df17fa974930fe262b7dcb815feeeebb22af9a
NPG418 <narupenguin919@gmail.com>
2024-05-21 13:27:02 +0900

    ibl調整

--- .config/nvim/lua/plugins/indent.lua
+++ .config/nvim/lua/plugins/indent.lua
@@ -7,29 +7,31 @@ return {
     'HiPhish/rainbow-delimiters.nvim',
   },
   config = function()
-    local rainbow = {
-      RainbowRed = '#E06C75',
-      RainbowYellow = '#E5C07B',
-      RainbowBlue = '#61AFEF',
-      RainbowOrange = '#D19A66',
-      RainbowGreen = '#98C379',
-      RainbowViolet = '#C678DD',
-      RainbowCyan = '#56B6C2',
+    local highlight = {
+      RainbowRed = '#b33f48',
+      RainbowYellow = '#b8934e',
+      RainbowBlue = '#3482c2',
+      RainbowGreen = '#6b964c',
+      RainbowViolet = '#994bb0',
+      RainbowCyan = '#298995',
     }
 
     local hooks = require('ibl.hooks')
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      for hl, color in pairs(rainbow) do
+      for hl, color in pairs(highlight) do
         vim.api.nvim_set_hl(0, hl, { fg = color })
       end
-      vim.api.nvim_set_hl(0, 'IblScope', { fg = '#ffffff' })
     end)
 
-    vim.g.rainbow_delimiters = { highlight = vim.tbl_keys(rainbow) }
+    vim.g.rainbow_delimiters = { highlight = vim.tbl_keys(highlight) }
 
     require('ibl').setup({
-      scope = { show_start = false },
-      indent = { highlight = vim.tbl_keys(rainbow) },
+      scope = {
+        show_start = false,
+      },
+      indent = {
+        highlight = vim.tbl_keys(highlight)
+      },
     })
 
     hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
