26445e1d6c7d5ac7915bb7d486909d4ac3d2e6ab
NPG418 <narupenguin919@gmail.com>
2024-09-13 21:30:58 +0900

    cleanup

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -47,11 +47,6 @@ Get-ChildItem "$PROFILE\..\Completions\" | ForEach-Object {
     . $_.FullName
 }
 
-#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
-
 Import-Module -Name Microsoft.WinGet.CommandNotFound
-#f45873b3-b655-43a6-b217-97c00aa0db58
-
 
 if (Test-Path "C:\Users\nullp\.jabba\jabba.ps1") { . "C:\Users\nullp\.jabba\jabba.ps1" }
-
