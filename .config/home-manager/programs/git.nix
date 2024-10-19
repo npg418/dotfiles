2c0c05a5d6d9fa87057ba10441adcc257d90a3d1
NPG418 <narupenguin919@gmail.com>
2024-10-19 12:04:23 +0000

    gitのalias設定

--- .config/home-manager/programs/git.nix
+++ .config/home-manager/programs/git.nix
@@ -17,6 +17,10 @@
         "https://github.com".helper = "!gh auth git-credential";
         "https://gist.github.com".helper = "!gh auth git-credential";
       };
+      alias = {
+        root = "rev-parse --show-toplevel";
+        tree = "log --graph --oneline --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset";
+      };
     };
   };
 }
