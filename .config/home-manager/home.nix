8d680eb97b206d06881985dda7e07020cdf0a7c5
NPG418 <narupenguin919@gmail.com>
2024-10-20 10:49:30 +0000

    Format

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -63,9 +63,7 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = with pkgs; [
-    gh
-  ];
+  home.packages = with pkgs; [ gh ];
 
   programs.zoxide.enable = true;
 
--- nixos/configuration.partial.nix
+++ nixos/configuration.partial.nix
@@ -5,4 +5,3 @@
   users.users.nullp.shell = pkgs.zsh;
   i18n.defaultLocale = "ja_JP.UTF-8";
 }
-
