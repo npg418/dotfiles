9ec28d52c4cc10cac98a40f23ebbc007dc7b2611
NPG418 <narupenguin919@gmail.com>
2024-11-23 11:46:33 +0000

    install.shの更新

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ dot_dir=$(pwd)
 if ! [ -x "$(command -v nix)" ]; then
   echo "Nix not found! Installing..."
   sh <(curl -L https://nixos.org/nix/install) --no-daemon
+  echo "Please re-start your shell to ensure updated PATH applied."
+  exit 0
 fi
 
 if ! [ -x "$(command -v home-manager)" ]; then
@@ -15,6 +17,8 @@ if ! [ -x "$(command -v home-manager)" ]; then
   nix-channel --add "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz" home-manager
   nix-channel --update
   NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH} nix-shell "<home-manager>" -A install
+  echo "Please re-start your shell to ensure updated PATH applied."
+  exit 0
 fi
 
 echo "Copying home-manager configuration..."
