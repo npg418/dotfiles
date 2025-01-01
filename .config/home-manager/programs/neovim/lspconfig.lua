4e3cf02b1dd4f0b3ced9bf36691e178735d2996c
NPG418 <narupenguin919@gmail.com>
2025-01-01 21:24:00 +0900

    参照が変わってたのを対応

--- .config/home-manager/programs/neovim/lspconfig.lua
+++ .config/home-manager/programs/neovim/lspconfig.lua
@@ -51,12 +51,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "g.", vim.lsp.buf.code_action, { desc = "Do code action under cursor (Neovim builtin)" })
     vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover on symbol under cursor (Neovim builtin)" })
     vim.keymap.set("n", "ge", vim.diagnostic.open_float, { desc = "Open diagnostic float (Neovim builtin)" })
-    vim.keymap.set("n", "g]", function()
-      vim.diagnostic.jump({ count = 1, float = true })
-    end, { desc = "Goto next diagnostic (Neovim builtin)" })
-    vim.keymap.set("n", "g[", function()
-      vim.diagnostic.jump({ count = -1, float = true })
-    end, { desc = "Goto previous diagnostic (Neovim builtin)" })
+    vim.keymap.set("n", "g]", vim.diagnostic.goto_next, { desc = "Goto next diagnostic (Neovim builtin)" })
+    vim.keymap.set("n", "g[", vim.diagnostic.goto_prev, { desc = "Goto previous diagnostic (Neovim builtin)" })
 
     local client = vim.lsp.get_client_by_id(e.data.client_id)
     if client ~= nil and client.supports_method("textDocument/formatting") then
