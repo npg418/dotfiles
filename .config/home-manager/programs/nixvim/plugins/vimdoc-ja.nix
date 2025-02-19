811e7079b8e078044c62d3c1fffc647ae42f8644
NPG418 <narupenguin919@gmail.com>
2025-02-19 13:03:02 +0900

    helplangがうまく効いてなかった

--- .config/home-manager/programs/nixvim/plugins/vimdoc-ja.nix
+++ .config/home-manager/programs/nixvim/plugins/vimdoc-ja.nix
@@ -1,32 +1,18 @@
-{ pkgs, lib, ... }:
-let
-  fromGitHub =
-    {
-      owner,
-      repo,
-      rev,
-      ...
-    }@spec:
-    pkgs.vimUtils.buildVimPlugin {
-      pname = lib.strings.sanitizeDerivationName "${owner}/${repo}";
-      version = rev;
-      src = pkgs.fetchFromGitHub spec;
-    };
-in
+{ pkgs, ... }:
 {
   programs.nixvim = {
     extraPlugins = [
-      {
-        plugin = fromGitHub {
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "vimdoc-ja";
+        src = pkgs.fetchFromGitHub {
           owner = "vim-jp";
           repo = "vimdoc-ja";
           rev = "7e381730febb695fb58c9d6c8fd867226180ca2d";
           hash = "sha256-x3UxwC16jWqlaVBWGCdc6g5bLOZJMtd6fqTUkM5cedA=";
         };
-        config = ''
-          set helplang = "ja,en"
-        '';
-      }
+      })
     ];
+
+    opts.helplang = "ja,en";
   };
 }
