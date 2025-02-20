0885101d66fa8c22f0d07fd773b7d096cba4d0eb
NPG418 <narupenguin919@gmail.com>
2025-02-20 20:09:23 +0900

    direnvの設定の更新

--- .config/home-manager/programs/direnv.nix
+++ .config/home-manager/programs/direnv.nix
@@ -4,7 +4,8 @@
     enable = true;
     nix-direnv.enable = true;
   };
+  programs.gint.ignores = [ ".direnv/" ];
   programs.zsh.zsh-abbr.abbreviations = {
-    direnvinit = "nix flake new -t github:nix-community/nix-direnv";
+    direnvinit = "nix flake new -t github:nix-community/nix-direnv .";
   };
 }
