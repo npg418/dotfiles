99ed0e9391528922cc202051c19c32c9ecc674cb
NPG418 <narupenguin919@gmail.com>
2025-03-15 18:23:21 +0900

    direnvを設定

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Project configuration for npg418/dotfiles/windows";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { nixpkgs, systems, ... }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = eachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              treefmt
+              nixfmt-rfc-style
+              taplo
+            ];
+          };
+        }
+      );
+    };
+}
