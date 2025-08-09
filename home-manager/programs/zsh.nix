bb753db1a2edd2812fce377c7c25c78d58f7b7e4
NPG418 <narupenguin919@gmail.com>
2025-08-09 21:41:13 +0900

    ctrl-fでghqスイッチャを開く

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -1,4 +1,14 @@
 {
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  hasFzf = config.programs.fzf.enable;
+  hasGhq = builtins.elem pkgs.ghq config.home.packages;
+in
+{
   nixpkgs.config.allowUnfree = true;
   programs.zsh = {
     enable = true;
@@ -21,6 +31,18 @@
       enable = true;
       abbreviations.hm = "home-manager";
     };
+    initExtra = lib.mkIf (hasFzf && hasGhq) ''
+      function ghq_fzf_change_directory() {
+        local src=$(ghq list | fzf --preview "eza -l -g -a --icons $(ghq root)/{} | tail -n+4 | awk '{print \$6\"/\"\$8\" \"\$9 \" \" \$10}'")
+        if [ -n "$src" ]; then
+          BUFFER="cd $(ghq root)/$src"
+          zle accept-line
+        fi
+        zle -R -c
+      }
+      zle -N ghq_fzf_change_directory
+      bindkey '^f' ghq_fzf_change_directory
+    '';
     envExtra = ''
       if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then
         . ~/.nix-profile/etc/profile.d/nix.sh
