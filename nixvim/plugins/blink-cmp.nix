4c6e58f12cdc849d6c05fc909650e7f98520229b
NPG418 <narupenguin919@gmail.com>
2025-12-09 09:55:27 +0900

    copilot-cmpを導入

--- nixvim/plugins/blink-cmp.nix
+++ nixvim/plugins/blink-cmp.nix
@@ -1,11 +1,16 @@
+{ lib, ... }:
 {
   plugins.blink-cmp = {
     enable = true;
     settings = {
+      sources.default = lib.mkBefore [
+        "lsp"
+        "path"
+        "snippets"
+        "buffer"
+      ];
       completion.documentation.auto_show = true;
-      keymap = {
-        preset = "default";
-      };
+      keymap.preset = "default";
     };
   };
 }
--- nixvim/plugins/copilot.nix
+++ nixvim/plugins/copilot.nix
@@ -1,3 +1,16 @@
+{ lib, config, ... }:
 {
-  plugins.copilot-lua.enable = true;
+  plugins = {
+    copilot-lua.enable = true;
+    blink-copilot.enable = config.plugins.blink-cmp.enable;
+    blink-cmp.settings.sources = {
+      default = lib.mkAfter [ "copilot" ];
+      providers.copilot = {
+        name = "copilot";
+        module = "blink-copilot";
+        score_offset = 100;
+        async = true;
+      };
+    };
+  };
 }
