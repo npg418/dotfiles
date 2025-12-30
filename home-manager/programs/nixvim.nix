1243fcdd2c759233fae8ccdeadb362970bf5d1cf
NPG418 <narupenguin919@gmail.com>
2025-12-30 21:47:55 +0900

    HMSwitchコマンド

--- home-manager/programs/nixvim.nix
+++ home-manager/programs/nixvim.nix
@@ -1,10 +1,21 @@
 {
   programs = {
-    nixvim = {
-      enable = true;
-      defaultEditor = true;
-      vimdiffAlias = true;
-    };
+    nixvim =
+      { lib, ... }:
+      {
+        enable = true;
+        defaultEditor = true;
+        vimdiffAlias = true;
+
+        userCommands.HMSwitch = {
+          command = lib.nixvim.mkRaw ''
+            function()
+              vim.fn.system("home-manager switch")
+              vim.cmd.source("$MYVIMRC")
+            end
+          '';
+        };
+      };
 
     zsh.zsh-abbr.abbreviations.v = "nvim";
   };
