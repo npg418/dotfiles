f3f44ff166c67da6990e5d37c6c094d8536a8bdc
NPG418 <narupenguin919@gmail.com>
2026-06-06 23:32:51 +0900

    そういやまだnixos-wsl 26.05対応してなかった

--- templates/nixos-configuration/flake.nix
+++ templates/nixos-configuration/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
     dotfiles = {
       url = "github:npg418/dotfiles?dir=nixos";
       inputs.nixpkgs.follows = "nixpkgs";
